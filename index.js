eeefabbe4147c3d665755b496de86007438b0395
Drew Miller <drew.miller@songkick.com>
2023-01-31 15:04:58 +0000

    morphing to new version

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const fs = require ('fs');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const fingerprintPath = core.getInput('path') || "./.expo/fingerprint.json";
+  console.log(`Hello ${fingerprintPath}!`);
+
+  const rawdata = fs.readFileSync(fullPath);
+  const rootObj = JSON.parse(rawdata);
+
+  console.log({rootObj});
+  console.log(`hash is: ${rootObj.hash}`);
+  const fingerprint = (new Date()).toTimeString();
+  core.setOutput("fingerprint", fingerprint);
 } catch (error) {
   core.setFailed(error.message);
 }
