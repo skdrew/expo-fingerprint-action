c0fb3fcb5b71b0cb9fd55535989dfd439ca2e2a8
Drew Miller <drew.miller@songkick.com>
2023-01-31 15:20:05 +0000

    remove debug

--- index.js
+++ index.js
@@ -4,18 +4,12 @@ const fs = require ('fs');
 
 try {
     const fingerprintPath = core.getInput('path') || "./.expo/fingerprint.json";
-
     const fullPath = path.resolve(fingerprintPath);
-    core.info(`Processing file: ${fullPath}`);
-    console.log(`Hello ${fullPath}!`);
 
     const rawdata = fs.readFileSync(fullPath);
     const rootObj = JSON.parse(rawdata);
 
-    console.log({rootObj});
-    console.log(`hash is: ${rootObj.hash}`);
-    const fingerprint = (new Date()).toTimeString();
-    core.setOutput("fingerprint", fingerprint);
+    core.setOutput("fingerprint", rootObj.hash);
 } catch (error) {
     core.setFailed(error.message);
 }
